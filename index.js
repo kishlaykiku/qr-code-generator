6c7af7d5d4f7d03c78027d832af8697dc3cc654c
kishlay <kishlaykumarkkp24@gmail.com>
2023-06-14 01:08:31 +0530

    pushed

--- index.js
+++ index.js
@@ -3,3 +3,19 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+import inquirer from "inquirer";
+
+inquirer
+  .prompt([
+    'Enter address: '
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
