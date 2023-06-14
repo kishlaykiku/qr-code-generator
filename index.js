982f1a42c8936fceb2ddaf79f10c1456d7070dce
kikutestground <hodingkishlay@gmail.com>
2023-06-15 00:26:40 +0530

    validation

--- index.js
+++ index.js
@@ -4,18 +4,39 @@
 3. Create a txt file to save the user input using the native fs node module.
 */
 import inquirer from "inquirer";
+import fs from "fs";
 
 inquirer
-  .prompt([
-    'Enter address: '
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+    .prompt([
+        /* Pass your questions in here */
+        {
+            name: 'address',
+            message: 'Enter Address: ',
+            type: 'input',
+            validate(answers)
+            {
+                // var re = new RegExp('(http|https)://(www.)?');
+                if(!answers)
+                {
+                    return "Enter valid address";
+                }
+                return true;
+            }
+        }
+    ])
+    .then((answers) => 
+    {
+        // Use user feedback for... whatever!!
+        console.log(answers.address);
+    })
+    .catch((error) => 
+    {
+        if (error.isTtyError) 
+        {
+            // Prompt couldn't be rendered in the current environment
+        } 
+        else 
+        {
+            // Something else went wrong
+        }
+    });
